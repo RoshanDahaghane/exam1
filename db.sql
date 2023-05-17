689409829ae54687866069b62a319e32729f54de
dac <dac@dypm.org>
2023-05-17 09:27:13 +0530

    exam

---
+++ db.sql
@@ -0,0 +1,19 @@
+create table customer(
+name varchar(30),
+ph.no. varchar(30) unique,
+email_id varchar(30) primary key
+);
+
+create table office (
+loc varchar(30) primary key,
+name varchar(30) 
+);
+
+create table counter(
+name varchar(30),
+cr.no. int primary key
+);
+create table canteen (
+room no int ,
+roomid int primary key()
+)
